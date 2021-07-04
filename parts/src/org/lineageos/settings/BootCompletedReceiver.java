8e78788d0ab63c2d6307cf3cb3b874fef96c3d5a
Ramii Ahmed <ramy@ahmedramy.com>
2021-07-04 12:53:33 +0000

    munch: parts: Initialize High Touch Polling Rate support
    Used to handle our node to force enable Xiaomi's High Touch polling rate mode from kernel
    
    Adapted High Touch Polling Rate support to S style

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -29,6 +29,7 @@ import androidx.preference.PreferenceManager;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -46,5 +47,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         RefreshUtils.startService(context);
         boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
         FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+        TouchSamplingUtils.restoreSamplingValue(context);
     }
 }
