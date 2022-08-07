ddf95c92a6379c7c2f3e1e088c8e4deac41617c8
madmax7896 <madmax78968@gmail.com>
2022-08-07 19:43:47 +0530

    munch: change for munch(POCO F4)

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -12,14 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+$(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := lineage_alioth
-PRODUCT_DEVICE := alioth
+PRODUCT_NAME := lineage_munch
+PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_MODEL := POCO F4
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.8.0.TKHMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/munch_global/munch:13/RKQ1.211001.001/V14.0.6.0.TLMMIXM:user/release-keys
