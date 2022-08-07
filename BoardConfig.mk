1584daba97e3940e124fe30c73b27ed8530b9421
madmax7896 <madmax78968@gmail.com>
2022-08-07 19:43:47 +0530

    munch: change for munch (Redmi K40S/POCO F4)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@
 # Inherit from sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/xiaomi/alioth
+DEVICE_PATH := device/xiaomi/munch
 
 BUILD_BROKEN_DUP_RULES := true
 
@@ -18,17 +18,17 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_munch
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_munch
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/alioth.config
+TARGET_KERNEL_CONFIG := vendor/munch_defconfig
 
 # OTA assert
-TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
+TARGET_OTA_ASSERT_DEVICE := munch,munch_global,munchin
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
-include vendor/xiaomi/alioth/BoardConfigVendor.mk
+include vendor/xiaomi/munch/BoardConfigVendor.mk
