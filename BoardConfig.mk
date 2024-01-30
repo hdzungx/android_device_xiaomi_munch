e9f0bd054b2de4b4f2a9dd85d4ed608d1af701ad
Georg Veichtlbauer <georg@vware.at>
2024-01-30 09:29:36 +0100

    munch: powerhint: Add DT2W
    Change-Id: I7d0dfe249798864a39afd4f339b692584faaf3ec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,9 +143,6 @@ BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
