2afaa4745676ccd10de2e9da8227d90f83ce3c1e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-05-27 23:34:08 +0200

    munch: Drop selinux context from mi_thermald init unit
    We do that from sepolicy label
    
    Change-Id: I56fb68b8121a68927cb98e494263f092732bd8ed

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup() {
         vendor/etc/init/init.batterysecret.rc)
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
+        vendor/etc/init/init.mi_thermald.rc)
+            sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
