4d726f61a20a57f59f21c60386359d9b189c2ae7
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-02 18:35:29 +0200

    munch: Rename prebuilt audio HAL and libs
    Change-Id: Ia46a5ba431767795135f058f092aa38999e860ae

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup() {
         vendor/etc/init/init.mi_thermald.rc)
             sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
             ;;
+        vendor/lib/hw/audio.primary.munch.so)
+            sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_munch\.so\x00\x00\x00\x00\x00\x00" "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
