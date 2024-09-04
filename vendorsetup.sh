add1263cfd7af7f885b386455f8af78ee9f3860b
HDzungx <hdzungx@gmail.com>
2024-09-04 11:30:35 +0700

    munch: Switch to Neutron Clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,13 @@ if [ -d "kernel/xiaomi/munch/KernelSU" ]; then
     rm -rf KernelSU/userspace/su
     cd ../../..
 fi
+
+if [ ! -d "prebuilts/clang/host/linux-x86/clang-neutron" ]; then
+    cd prebuilts/clang/host/linux-x86
+    mkdir clang-neutron
+    cd clang-neutron
+    curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
+    chmod +x antman
+    ./antman -S=05012024
+    cd ../../../../..
+fi
