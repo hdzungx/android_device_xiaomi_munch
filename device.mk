b95caa5a29e3bd4cc25e27606feba86cd991fb0b
HDzungx <hdzungx@gmail.com>
2024-10-14 00:29:26 +0700

    munch: Remove unwanted packages

--- device.mk
+++ device.mk
@@ -334,6 +334,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
